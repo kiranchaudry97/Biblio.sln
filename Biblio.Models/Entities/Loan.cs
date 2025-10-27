4af956244701b10722f7deca54f22f1890de0e1f
kiranchaudry97 <kiran.chaudry@outlook.be>
2025-10-27 22:07:50 +0100

    Overzicht van Loan.cs • Klasse: Loan : BaseEntity • Doel: model voor uitleningen met koppeling naar boek en lid • Eigenschappen: • BookId (int), Book (Book?) • MemberId (int), Member (Member?) • StartDate (DateTime), DueDate (DateTime), ReturnedAt (DateTime?) • IsClosed (bool, afgeleid van ReturnedAt.HasValue) • Soft-delete: via BaseEntity (IsDeleted, DeletedAt); gefilterd met global query filters in DbContext • Mapping (in DbContext): • Tabel: Uitleningen • Kolommen: UitleningId, BoekId, LidId, StartDatum, EindDatum, IngeleverdOp • Indexen/regels (in DbContext): • Unieke gefilterde index: BoekId met filter [IngeleverdOp] IS NULL (max. één actieve uitlening per boek)

--- Biblio.Models/Entities/Loan.cs
+++ Biblio.Models/Entities/Loan.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Doel: Uitlening-entiteit met relaties naar Boek en Lid, data voor periodes en status.
+// Beschrijving: Bewaart start-, eind- en inleverdatum en koppelt boek en lid; heeft IsClosed afgeleid van ReturnedAt.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,18 +11,18 @@ namespace Biblio.Models.Entities;
 
 public class Loan : BaseEntity
 {
-    public int BookId { get; set; }
-    public Book? Book { get; set; }
+ public int BookId { get; set; }
+ public Book? Book { get; set; }
 
 
-    public int MemberId { get; set; }
-    public Member? Member { get; set; }
+ public int MemberId { get; set; }
+ public Member? Member { get; set; }
 
 
-    public DateTime StartDate { get; set; }
-    public DateTime DueDate { get; set; }
-    public DateTime? ReturnedAt { get; set; }
+ public DateTime StartDate { get; set; }
+ public DateTime DueDate { get; set; }
+ public DateTime? ReturnedAt { get; set; }
 
 
-    public bool IsClosed => ReturnedAt.HasValue;
+ public bool IsClosed => ReturnedAt.HasValue;
 }
