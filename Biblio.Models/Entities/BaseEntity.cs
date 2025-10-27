ec4f1318da9db5deaed80d6b3fe65c7bd17b1fc0
kiranchaudry97 <kiran.chaudry@outlook.be>
2025-10-27 22:04:28 +0100

    Overzicht van BaseEntity.cs • Klasse: abstracte BaseEntity • Eigenschappen: Id (int), IsDeleted (bool), DeletedAt (DateTime?) • Doel: soft-delete basis voor alle entiteiten • Integratie: DbContext hanteert global query filters op IsDeleted == false

--- Biblio.Models/Entities/BaseEntity.cs
+++ Biblio.Models/Entities/BaseEntity.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Doel: Basisklasse voor entiteiten met soft-delete ondersteuning (IsDeleted, DeletedAt).
+// Beschrijving: Biedt Id, IsDeleted en DeletedAt voor logische verwijderingen op alle afgeleide entiteiten.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +11,7 @@ namespace Biblio.Models.Entities;
 
 public abstract class BaseEntity
 {
-    public int Id { get; set; }
-    public bool IsDeleted { get; set; }
-    public DateTime? DeletedAt { get; set; }
+ public int Id { get; set; }
+ public bool IsDeleted { get; set; }
+ public DateTime? DeletedAt { get; set; }
 }
